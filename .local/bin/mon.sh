1e0b76c35f59ce07711b7c5c8c42039922d05be6
Koushik <koushikhr1441@gmail.com>
2024-01-30 23:49:46 +0530

    garuda hyprland configs

---
+++ .local/bin/mon.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+hyprctl monitors > /tmp/monitor
+
+var=$(sed -n '1{p;q}' /tmp/monitor | awk '{ print $2 }')
+
+var1=$(sed -n '2{p;q}' /tmp/monitor | awk '{ print $1 }')
+
+var2=$(sed -n '2{p;q}' /tmp/monitor | awk '{ print $3 }')
+
+var3=$(sed -n '10{p;q}' /tmp/monitor | awk '{ print $2 }')
+
+var4="$var, $var1, $var2, $var3"
+
+sed -i "/monitor=/c\monitor= $var4" .config/hypr/hyprland.conf
